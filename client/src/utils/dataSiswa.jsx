3b06b8df0b5c8608021309ea3b860453ab984135
RearizthMD <astarohhellprince@gmail.com>
2024-03-31 22:17:44 +0700

    add data to kelolosan banner

--- client/src/utils/dataSiswa.jsx
+++ client/src/utils/dataSiswa.jsx
@@ -4,55 +4,134 @@ import siswa3 from "../assets/imgSiswa/Mask group (7).png"
 import siswa4 from "../assets/imgSiswa/Mask group (8).png"
 import siswa5 from "../assets/imgSiswa/Mask group (9).png"
 import siswa6 from "../assets/imgSiswa/Mask group (15).png"
+import adrianD from "../assets/imgSiswa/Institute Teknologi Bandung.png"
+import azzahraHJ from "../assets/imgSiswa/ITB - Azzahra Hasfa.png"
+import dzakiFR from "../assets/imgSiswa/ITB - Dzaki Fachri.png"
+import nurinta from "../assets/imgSiswa/ITB - Nurinta.png"
+import najma from "../assets/imgSiswa/ITB - Najma Audrey.png"
+import nauval from "../assets/imgSiswa/ITB - M. Nauval Naeem.png"
+import hanny from "../assets/imgSiswa/UGM - Hanny.png"
+import sarah from "../assets/imgSiswa/UGM - Sarah Fahira.png"
+import jescenia from "../assets/imgSiswa/UGM (1).png"
+
 
 
 export const data_Siswa = [
 
+  // {
+  //   img: siswa1,
+  //   nama: "Anita",
+  //   asalSekolah: "SMAN 4 Jakarta",
+  //   kuliah: "Farmasi - Poltekes JKT II",
+  //   desk: "kalo kalian bingung mau kuliah kemana, tenang ada pa badru yg siap sedia mengarahkan. kalo kalian mumet sama mtk, ada ka arya siap bantuin jawab soal mtk yg diluar nalar itu. kalo kalian gangerti biologi, ada ka firyal siap ngejelasin berulang ulang. kalo kalian gasuka fisika, ada ka wau yg bikin fisika jadi gampang dan seru. kalo kalian pusing kimia, ada ka alvin sepuh kimia. jangan lupa log in sinteta guys😜😜😜😇😇🫰🏻🫰🏻🫰🏻",
+  //   },
+  //   {
+  //   img: siswa2,
+  //   nama: "Surya Fatta Hadian",
+  //   asalSekolah:"SMAN Muhammadiyah 23 Jakarta",
+  //   kuliah: "Sanitasi Lingkungan -Poltekes JKT II II",
+  //   desk: "dari segi tentornya enak bgt bgt bgt karena bisa berbaur sama murid muridnya, tentornya ga yg mengintimidasi alias chill🤓. kalo cara ngajar enak juga sih kita diajarin sampe paham terus sabar (ka alvin ngga guys karna dia kl dikatain ngatain balik😇). RANKING KU DARI 20 BESAR JADI 10 BESAR SELAMA BELAJAR DI SINTETA🫰🏻🫰🏻🫰🏻🫰🏻🫰🏻🫰🏻🫰🏻. kalo kalian bingung mau kuliah kemana, tenang ada pa badru yg siap sedia mengarahkan. kalo kalian mumet sama mtk, ada ka arya siap bantuin jawab soal mtk yg diluar nalar itu. kalo kalian gangerti biologi, ada ka firyal siap ngejelasin berulang ulang. kalo kalian gasuka fisika, ada ka wau yg bikin fisika jadi gampang dan seru. kalo kalian pusing kimia, ada ka alvin sepuh kimia. jangan lupa log in sinteta guys😜😜😜😇😇🫰🏻🫰🏻🫰🏻 sekian ripiu dari si imut nan cantik🥰 "
+  //   ,
+  //   },
+  //   {
+  //   img: siswa3,
+  //   nama: "jescenia Shaquila",
+  //   asalSekolah:"SMA ANGKASA 1 JKT" ,
+  //   kuliah:"Ilmu Komunikasi-UGM ",
+  //   desk: "haloo saya Shaqiulla Alhamdulillah saya telah lolos di salah satu Univ favorit yaitu UGM, pertama saya ucapkan terimakasih buat kaka-kaka sinteta yang udah bimbing saya saat belajar di sinteta, terutama buat pak Badru saya sangat berterimakasih karena sudah mengarahkan saya ke unif dan jurusan yang cocok sesuai keahlian saya. dan karna sebelumnya saya masih bingung mau ambil jurusan apa yang sesuai sama pasion saya, Alhamdulillah setelah di arahkan sama pak Badru saya jadi tau pasion saya lebih ke arah mana.. makasih pa Badru makasih juga kaka-kaka sinteta"
+    
+    
+  //   },
+  //   {
+  //   img: siswa4,
+  //   nama:"Alyssa Parawansyah" ,
+  //   asalSekolah:"SMAN 114 Jakarta",
+  //   kuliah:"Farmasi-PolketesJKT II",
+  //   desk: "Alhamdulillah, Puji Syukur aku telah Lolos SNBP di Kampus Favorit karena Telah diarahkan dalam Target Nilai Raport dari kelas 10 hingga kelas 12",
+    
+  //   },
+  //   {
+  //   img: siswa5,
+  //   nama: "Ammar",
+  //   asalSekolah:  "SMAN 114 Jakarta",
+  //   kuliah: "Farmasi-PolketesJKT II",
+  //   desk: "Hallo nama saya Ammar, Alhamdulillah sudah  Lolos di Poltekkes dan bisa memaksimalkan Nilai Raport saya di luar Jalur SNBP",
+  //   },
+  //   {
+  //   img: siswa6,
+  //   nama: "Zahwa Indah Pratiwi",
+  //   asalSekolah:"SMAN 50 Jakarta",
+  //   kuliah:"Analisis Farmasi  & Makanan Poltekes Jakarta II",
+  //   desk: "dari segi tentornya enak bgt bgt bgt karena bisa berbaur sama murid muridnya, tentornya ga yg mengintimidasi alias chill🤓.",
+  //   },
+
   {
-    img: siswa1,
-    nama: "Anita",
-    asalSekolah: "SMAN 4 Jakarta",
-    kuliah: "Farmasi - Poltekes JKT II",
+    img: jescenia,
+    nama: "Jescenia Shaquila",
+    asalSekolah: "SMAN ANGKASA JKT",
+    kuliah: "ILMU KOMUNIKASI - UGM",
     desk: "kalo kalian bingung mau kuliah kemana, tenang ada pa badru yg siap sedia mengarahkan. kalo kalian mumet sama mtk, ada ka arya siap bantuin jawab soal mtk yg diluar nalar itu. kalo kalian gangerti biologi, ada ka firyal siap ngejelasin berulang ulang. kalo kalian gasuka fisika, ada ka wau yg bikin fisika jadi gampang dan seru. kalo kalian pusing kimia, ada ka alvin sepuh kimia. jangan lupa log in sinteta guys😜😜😜😇😇🫰🏻🫰🏻🫰🏻",
     },
     {
-    img: siswa2,
-    nama: "Surya Fatta Hadian",
-    asalSekolah:"SMAN Muhammadiyah 23 Jakarta",
-    kuliah: "Sanitasi Lingkungan -Poltekes JKT II II",
+    img: hanny,
+    nama: "Hanny Maurize Suei",
+    asalSekolah:"SMAN 71 JKT",
+    kuliah: "S.INFORMASI GEOGRAFIS - UGM",
     desk: "dari segi tentornya enak bgt bgt bgt karena bisa berbaur sama murid muridnya, tentornya ga yg mengintimidasi alias chill🤓. kalo cara ngajar enak juga sih kita diajarin sampe paham terus sabar (ka alvin ngga guys karna dia kl dikatain ngatain balik😇). RANKING KU DARI 20 BESAR JADI 10 BESAR SELAMA BELAJAR DI SINTETA🫰🏻🫰🏻🫰🏻🫰🏻🫰🏻🫰🏻🫰🏻. kalo kalian bingung mau kuliah kemana, tenang ada pa badru yg siap sedia mengarahkan. kalo kalian mumet sama mtk, ada ka arya siap bantuin jawab soal mtk yg diluar nalar itu. kalo kalian gangerti biologi, ada ka firyal siap ngejelasin berulang ulang. kalo kalian gasuka fisika, ada ka wau yg bikin fisika jadi gampang dan seru. kalo kalian pusing kimia, ada ka alvin sepuh kimia. jangan lupa log in sinteta guys😜😜😜😇😇🫰🏻🫰🏻🫰🏻 sekian ripiu dari si imut nan cantik🥰 "
     ,
     },
     {
-    img: siswa3,
-    nama: "jescenia Shaquila",
-    asalSekolah:"SMA ANGKASA 1 JKT" ,
-    kuliah:"Ilmu Komunikasi-UGM ",
+    img: sarah,
+    nama: "Sarah Fahira Gartika",
+    asalSekolah:"SMA 71 JKT" ,
+    kuliah:"PSIKOLOGI - UGM ",
     desk: "haloo saya Shaqiulla Alhamdulillah saya telah lolos di salah satu Univ favorit yaitu UGM, pertama saya ucapkan terimakasih buat kaka-kaka sinteta yang udah bimbing saya saat belajar di sinteta, terutama buat pak Badru saya sangat berterimakasih karena sudah mengarahkan saya ke unif dan jurusan yang cocok sesuai keahlian saya. dan karna sebelumnya saya masih bingung mau ambil jurusan apa yang sesuai sama pasion saya, Alhamdulillah setelah di arahkan sama pak Badru saya jadi tau pasion saya lebih ke arah mana.. makasih pa Badru makasih juga kaka-kaka sinteta"
     
     
     },
     {
-    img: siswa4,
-    nama:"Alyssa Parawansyah" ,
-    asalSekolah:"SMAN 114 Jakarta",
-    kuliah:"Farmasi-PolketesJKT II",
+    img: adrianD,
+    nama:"Adrian Daniel P" ,
+    asalSekolah:"SMAN 13 JKT",
+    kuliah:"FTI - ITB",
     desk: "Alhamdulillah, Puji Syukur aku telah Lolos SNBP di Kampus Favorit karena Telah diarahkan dalam Target Nilai Raport dari kelas 10 hingga kelas 12",
     
     },
     {
-    img: siswa5,
-    nama: "Ammar",
-    asalSekolah:  "SMAN 114 Jakarta",
-    kuliah: "Farmasi-PolketesJKT II",
+    img: nurinta,
+    nama: "Nurinta Altaf Fathiya",
+    asalSekolah:  "SMAN ANGKASA JKT",
+    kuliah: "FMIPA - ITB",
     desk: "Hallo nama saya Ammar, Alhamdulillah sudah  Lolos di Poltekkes dan bisa memaksimalkan Nilai Raport saya di luar Jalur SNBP",
     },
     {
-    img: siswa6,
-    nama: "Zahwa Indah Pratiwi",
-    asalSekolah:"SMAN 50 Jakarta",
-    kuliah:"Analisis Farmasi  & Makanan Poltekes Jakarta II",
+    img: azzahraHJ,
+    nama: "Azzahra Hasfa Julia",
+    asalSekolah:"SMAN 110 JKT",
+    kuliah:"HAYATI - ITB",
     desk: "dari segi tentornya enak bgt bgt bgt karena bisa berbaur sama murid muridnya, tentornya ga yg mengintimidasi alias chill🤓.",
     },
+    {
+      img: najma,
+      nama: "Najma Audrey Melodia",
+      asalSekolah:"SMAN 52 JKT",
+      kuliah:"MANAJEMN - ITB",
+      desk: "dari segi tentornya enak bgt bgt bgt karena bisa berbaur sama murid muridnya, tentornya ga yg mengintimidasi alias chill🤓.",
+      },
+      {
+        img:nauval,
+        nama: "M.Nauval Naeem",
+        asalSekolah:"SMAN 20 JKT",
+        kuliah:"FMIPA - ITB",
+        desk: "dari segi tentornya enak bgt bgt bgt karena bisa berbaur sama murid muridnya, tentornya ga yg mengintimidasi alias chill🤓.",
+        },
+        {
+          img: dzakiFR,
+          nama: "Dzaki Facri Rusadhi",
+          asalSekolah:"SMAN 77 JKT",
+          kuliah:"FTIB - ITB",
+          desk: "dari segi tentornya enak bgt bgt bgt karena bisa berbaur sama murid muridnya, tentornya ga yg mengintimidasi alias chill🤓.",
+          },
+          
 
 ]
